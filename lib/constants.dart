f2e87b60998cbebaab95c8121f5a930e58482378
Jack R <rubacha.jack03@gmail.com>
2023-05-25 23:30:43 -0400

    init web

--- lib/constants.dart
+++
@@ -1,9 +0,0 @@
-final class ProjectConstants {
-  static const String settingsBoxKey = 'settingsv1';
-
-  static const String darkModeStorageKey =
-      'darkMode'; // bool :  darkMode = true (dark) darkMode = false (light) darkMode = not present (System)
-
-  static const String colorSchemeStorageKey = 'colorScheme';
-
-}
